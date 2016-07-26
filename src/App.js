bb7c2890dc5e36799d60815d6e0a100a0cd7c6b1
Kamran AB Syed <1205abkamran@gmail.com>
2016-07-27 00:11:50 +0530

    Added the initial js files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import react, { Component } from 'react';
+
+const App extends Component {
+
+	render() {
+		return (
+				<div>
+					<a href="https://www.github.com" ></a>
+				</div>
+			);
+	}
+
+}
+
+export default App;
